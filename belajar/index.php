85bfc0c3a3dfa99a87787e06b974c2db06ed0852
diotivano <diotivano@gmail.com>
2017-05-02 11:19:19 +0700

    Belajar include dan require once

---
+++ belajar/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Sisfo Untan</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<h1>Sistem Informasi</h1>
+	<?php 
+		include_once 'tabel.php';
+		include_once 'satu.php';
+
+	 ?>
+	<p>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	</p>
+	<?php 
+	include 'tabel.php';
+	 ?>
+</body>
+</html>
---
+++ belajar/tabel.php
@@ -0,0 +1,17 @@
+	<table border="1" cellpadding="5" cellspacing="0" width="25%">
+		<caption>MAHASISWA</caption>
+		<thead>
+			<tr>
+				<th>No</th>
+				<th>NIM</th>
+				<th>Nama</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr align="center">
+				<td>1</td>
+				<td>H1101151016</td>
+				<td>Dio Tivano</td>
+			</tr>
+		</tbody>
+	</table>
