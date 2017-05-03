0956dd0dca617cf52a3503b70c03a83f3827ac4b
diotivano <diotivano@gmail.com>
2017-05-03 08:45:56 +0700

    Latihan

--- administrator/template/sidebar.php
+++ administrator/template/sidebar.php
@@ -1,27 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>SB Admin 2 - Bootstrap Admin Theme</title>
-
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-
-    <link href="assets/css/metisMenu.min.css" rel="stylesheet">
-
-    <link href="assets/css/sb-admin-2.css" rel="stylesheet">
-
-    <link href="assets/fonts/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-
-</head>
-
 <body>
 
             <div class="navbar-default sidebar" role="navigation">
